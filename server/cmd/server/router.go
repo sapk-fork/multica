18072dad2d6531ffe1345e34b1a7746a93a0ad15
Go Developer <sapk@sapk.fr>
2026-07-13 04:52:38 +0000

    style(export): gofmt export route registration in router

--- server/cmd/server/router.go
+++ server/cmd/server/router.go
@@ -1018,7 +1018,7 @@ func NewRouterWithOptions(pool *pgxpool.Pool, hub *realtime.Hub, bus *events.Bus
 					r.Put("/metadata/{key}", h.SetIssueMetadataKey)
 					r.Delete("/metadata/{key}", h.DeleteIssueMetadataKey)
 					r.Get("/pull-requests", h.ListPullRequestsForIssue)
-				r.Get("/export", h.ExportIssue)
+					r.Get("/export", h.ExportIssue)
 				})
 			})
 
