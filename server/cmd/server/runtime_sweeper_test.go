d276953375a97edca0a81f3b5af03d91f046b3fc
Full-stack Engineer <sapk@sapk.fr>
2026-07-15 06:14:40 +0000

    refactor(runtime): apply roast improvements to session-limit hold

--- server/cmd/server/runtime_sweeper_test.go
+++ server/cmd/server/runtime_sweeper_test.go
@@ -881,7 +881,7 @@ func TestSweepExpiredHolds(t *testing.T) {
 	})
 
 	queries := db.New(testPool)
-	sweepExpiredHolds(ctx, queries, nil, bus)
+	sweepExpiredHolds(ctx, queries, bus)
 
 	// Hold should be cleared.
 	var holdAfter *string
@@ -940,7 +940,7 @@ func TestSweepExpiredHoldsLeavesActiveholdsUntouched(t *testing.T) {
 	})
 
 	queries := db.New(testPool)
-	sweepExpiredHolds(ctx, queries, nil, nil)
+	sweepExpiredHolds(ctx, queries, nil)
 
 	// Hold should still be set.
 	var holdUntilSet bool
